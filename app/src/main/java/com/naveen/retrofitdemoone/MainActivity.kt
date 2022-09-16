572b5971b100ddf7012ed5dfc7705291c4d13add
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-16 17:39:57 +0530

    Display jsonData in Logcat

--- app/src/main/java/com/naveen/retrofitdemoone/MainActivity.kt
+++ app/src/main/java/com/naveen/retrofitdemoone/MainActivity.kt
@@ -2,10 +2,32 @@ package com.naveen.retrofitdemoone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val retService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+        val responseLiveData: LiveData<Response<Album>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList:MutableListIterator<AlbumItem>? = it.body()?.listIterator()
+            if(albumsList!=null){
+                while (albumsList.hasNext()){
+                    val albumItem = albumsList.next()
+                    Log.i("MYTAG", albumItem.title)
+                }
+            }
+        })
+
     }
 }
