54f95b1514a89c8b67bae2f04f83094d6b10edbc
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-17 13:42:27 +0530

    Display Query Parameters

--- app/src/main/java/com/naveen/retrofitdemoone/AlbumService.kt
+++ app/src/main/java/com/naveen/retrofitdemoone/AlbumService.kt
@@ -2,8 +2,12 @@ package com.naveen.retrofitdemoone
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AlbumService {
     @GET("/albums")
     suspend fun getAlbums() : Response<Album>
+
+    @GET("/albums")
+    suspend fun getSortedAlbums(@Query("userId")userId:Int) : Response<Album>
 }
--- app/src/main/java/com/naveen/retrofitdemoone/MainActivity.kt
+++ app/src/main/java/com/naveen/retrofitdemoone/MainActivity.kt
@@ -3,31 +3,42 @@ package com.naveen.retrofitdemoone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
+import com.naveen.retrofitdemoone.databinding.ActivityMainBinding
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         val retService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
         val responseLiveData: LiveData<Response<Album>> = liveData {
-            val response = retService.getAlbums()
+            val response = retService.getSortedAlbums(3)
             emit(response)
         }
+
         responseLiveData.observe(this, Observer {
             val albumsList:MutableListIterator<AlbumItem>? = it.body()?.listIterator()
             if(albumsList!=null){
                 while (albumsList.hasNext()){
                     val albumItem = albumsList.next()
                     Log.i("MYTAG", albumItem.title)
+                    val result = " "+"Album Title : ${albumItem.title}"+"\n"
+                    " "+"Album id : ${albumItem.id}"+"\n\n"
+                    " "+"User Id : ${albumItem.userId}"+"\n\n\n"
+                    binding.textView.append(result)
                 }
             }
         })
+
 
     }
 }
