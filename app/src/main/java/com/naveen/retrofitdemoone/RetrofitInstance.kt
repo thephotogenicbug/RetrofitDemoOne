bd884765fcfab6f056b12eba7702589f63c66c81
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-17 14:25:39 +0530

    Create OkHttpClient Builder

--- app/src/main/java/com/naveen/retrofitdemoone/RetrofitInstance.kt
+++ app/src/main/java/com/naveen/retrofitdemoone/RetrofitInstance.kt
@@ -1,20 +1,28 @@
 package com.naveen.retrofitdemoone
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
     companion object{
       val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+        val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+        }.build()
       fun getRetrofitInstance():Retrofit{
           return Retrofit.Builder()
               .baseUrl(BASE_URL)
+              .client(client)
               .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
               .build()
       }
-
-
 
     }
 }
